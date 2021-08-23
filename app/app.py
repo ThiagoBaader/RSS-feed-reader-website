d2783899ebd0c660913c4382c2e7c81c4929ffdd
Thiago Baader <thiago_andre_baader@hotmail.com>
2021-08-23 16:18:12 -0300

    Create app.py

---
+++ app/app.py
@@ -0,0 +1,29 @@
+# Library imports
+import feedparser
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def form():
+    return render_template('form.html')
+
+
+@app.route("/feed/", methods=['POST', 'GET'])
+def feed_reader():
+    if request.method == 'POST':
+        try:
+            feed_url = request.form['url']
+            feed = feedparser.parse(feed_url)
+            entries = []
+            entries.extend(feed.entries)
+
+            return render_template('feed_reader.html', entries=entries)
+
+        except:
+            return "The scraping job failed. Try again."
+
+
+if __name__ == "__main__":
+    app.run()
